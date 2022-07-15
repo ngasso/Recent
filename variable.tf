f3fba01b747e6d00f4395ce7c5e00a6b7f21766a
ngasso <103339990+ngasso@users.noreply.github.com>
2022-07-15 19:33:23 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
 variable "security_group" {
     type = list(number)
     default = [ 8000,9000,8080,80,22,23,443,360,300 ]
@@ -8,5 +12,5 @@ variable "instance_type" {
 }
 variable "ami" {
     type = string
-    default = "ami-0533f2ba8a1995cf9" 
+    default = "ami-0729e439b6769d6ab" 
 }
